7c8a1ee575ec2bededf9ae004fc6c2660a621ba5
Kareem ElZeky <kareem.elzeky@gmail.com>
2023-01-08 00:20:37 +0200

    removed action buttons from product cards when logged out

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { addToCart } from "../../features/Cart/CartSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { selectIsAuthenticated } from "../../features/Login/UserSlice";
 import "./ProductCard.css";
 
 const ProductCard = ({ product }) => {
-
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const isAuthentucated = useSelector(selectIsAuthenticated);
   const handleClick = () => {
     dispatch(addToCart(product.id));
     navigate("/cart");
@@ -19,12 +19,25 @@ const ProductCard = ({ product }) => {
       <div className="image-wrap">
         <img className="card-img" src={product.thumbnail} alt="productImage" />
       </div>
-      <p className="price">{(product.price - product.price * product.discountPercentage/100).toFixed(2)}$ <small><s>{product.price}$</s></small></p>
+      <p className="price">
+        {(
+          product.price -
+          (product.price * product.discountPercentage) / 100
+        ).toFixed(2)}
+        ${" "}
+        <small>
+          <s>{product.price}$</s>
+        </small>
+      </p>
       <p className="description">{product.description}</p>
-      <div className="card_actions">
-        <button className="btn" onClick = {handleClick}>Add to Cart</button>
-        <button className="btn">Buy Now</button>
-      </div>
+      {isAuthentucated &&
+        <div className="card_actions">
+          <button className="btn" onClick={handleClick}>
+            Add to Cart
+          </button>
+          <button className="btn">Buy Now</button>
+        </div>
+      }
     </div>
   );
 };
